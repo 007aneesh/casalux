b2112c2cb67da66240801e25c1d62555ebab1053
007aneesh <aneeshx000@gmail.com>
2026-04-30 21:45:18 +0530

    fix: db load

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -9,7 +9,7 @@ export const db =
     log: process.env['NODE_ENV'] === 'development' ? ['query', 'error', 'warn'] : ['error'],
   })
 
-if (process.env['NODE_ENV'] !== 'production') globalForPrisma.prisma = db
+globalForPrisma.prisma = db
 
 export { Prisma } from '../generated/prisma-client/index.js'
 export type { PrismaClient } from '../generated/prisma-client/index.js'
